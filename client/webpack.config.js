8039446f0d22b2465553227c6d8b2e2616d9c2d4
Sean Oh <seanoh@ucsb.edu>
2022-08-19 12:35:35 -0700

    set up webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const {GenerateSW} = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
@@ -11,17 +14,50 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, "dist")
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+     new HtmlWebpackPlugin({
+      template: "./index.html"
+     }),
+     new GenerateSW(),
+     new WebpackPwaManifest({
+      name: "Content Directory",
+      short_name: 'Directory',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ff0000',
+      start_url: './',
+      icons: [
+        {
+          src: path.resolve('./assets/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        }
+      ]
+     })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
     }
   };
 };
